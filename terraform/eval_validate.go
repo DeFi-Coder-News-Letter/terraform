b8b32735e0af7afca5f003c1873ba60a7cc2d166
Sherod Taylor <staylor@itbit.com>
2018-06-11 12:13:25 -0400

    added support for certificate based ssh

--- terraform/eval_validate.go
+++ terraform/eval_validate.go
@@ -254,6 +254,10 @@ var connectionBlockSupersetSchema = &configschema.Block{
 			Type:     cty.String,
 			Optional: true,
 		},
+		"certificate": {
+			Type:     cty.String,
+			Optional: true,
+		},
 		"host_key": {
 			Type:     cty.String,
 			Optional: true,
